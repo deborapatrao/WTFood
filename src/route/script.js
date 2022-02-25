e0644df1bf62993d5b0e8596ddc11a5f25161555
Debora Correia <deborapcorreia@gmail.com>
2022-02-24 20:38:08 -0800

    create search by ingredients with API and output into cards on HTML / Create a test result section on home to output the cards / create a new results page to, in the future, output the results

--- src/route/script.js
+++ src/route/script.js
@@ -8,5 +8,6 @@ Router.init("mainArea", [
   new Page("#community", "src/view/community.html"), // 1st Page is default if no URL match
   new Page("#profile", "src/view/profile.html"),
   new Page("#recipe", "src/view/recipe.html"),
+  new Page("#results", "src/view/results.html"),
   // add new pages here
 ]);
